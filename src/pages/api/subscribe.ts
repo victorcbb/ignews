6ec410da78e1082b5df0a0380bcb16403dff2dcd
Victor Barros <victorclovisb@hotmail.com>
2022-09-05 09:18:28 -0300

    Workin at Authentication with Next Auth

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { getSession } from "next-auth/react"
+import { stripe } from "../../services/stripe"
+
+export default async function handleSubscribeCheckout(
+  req:NextApiRequest, 
+  res:NextApiResponse) {
+  if(req.method === "POST") {
+    const session = await getSession({ req })
+
+    const striprCustomer = await stripe.customers.create({
+      email: session.user.email,
+      
+    })
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: striprCustomer.id,
+      payment_method_types: ["card"],
+      billing_address_collection: "required",
+      line_items: [
+        { price: 'price_1LcZqtLZ1P1UjdXub71bI3Qw', quantity: 1 }
+      ],
+      mode: "subscription",
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    })
+
+    return res.status(200).json({ seessionId: stripeCheckoutSession.id })
+  } else {
+    res.setHeader("Allow", "POST")
+    res.status(405).end("Method not allowed")
+  }
+}
