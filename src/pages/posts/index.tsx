72a632da7ad6f17df00e94c14cfbd89e498b6985
Victor Barros <victorclovisb@hotmail.com>
2022-09-06 10:40:07 -0300

    starting with primac dependencies

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head"
+import styles from "./styles.module.scss"
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Post | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 de agosto de 2022</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+          </a>
+          <a href="#">
+            <time>12 de agosto de 2022</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+          </a>
+          <a href="#">
+            <time>12 de agosto de 2022</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+          </a>
+        </div>
+      </main>
+    </>
+  )
+}
