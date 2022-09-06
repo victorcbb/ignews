b1fee3ac73f9e23b8272a585e659acdf8ee62c40
Victor Barros <victorclovisb@hotmail.com>
2022-09-06 09:48:42 -0300

    Finished integration with front and back-end

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,6 +1,17 @@
+import { query as q } from "faunadb"
 import { NextApiRequest, NextApiResponse } from "next"
 import { getSession } from "next-auth/react"
+import { fauna } from "../../services/fauna"
 import { stripe } from "../../services/stripe"
+
+type User = {
+  ref: {
+    id: string
+  }
+  data: {
+    stripe_customer_id: string
+  }
+}
 
 export default async function handleSubscribeCheckout(
   req:NextApiRequest, 
@@ -8,13 +19,38 @@ export default async function handleSubscribeCheckout(
   if(req.method === "POST") {
     const session = await getSession({ req })
 
-    const striprCustomer = await stripe.customers.create({
-      email: session.user.email,
-      
-    })
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index("user_by_email"),
+          q.Casefold(session.user.email)
+        )
+      )
+    )
+
+    let customerId = user.data.stripe_customer_id
+
+    if(!customerId) {
+      const striprCustomer = await stripe.customers.create({
+        email: session.user.email,
+      })
+    
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection("users"), user.ref.id),
+          {
+            data: {
+              stripe_customer_id: striprCustomer.id
+            }
+          }
+        )
+      )
+
+      customerId = striprCustomer.id
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: striprCustomer.id,
+      customer: customerId,
       payment_method_types: ["card"],
       billing_address_collection: "required",
       line_items: [
@@ -26,7 +62,7 @@ export default async function handleSubscribeCheckout(
       cancel_url: process.env.STRIPE_CANCEL_URL,
     })
 
-    return res.status(200).json({ seessionId: stripeCheckoutSession.id })
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id })
   } else {
     res.setHeader("Allow", "POST")
     res.status(405).end("Method not allowed")
